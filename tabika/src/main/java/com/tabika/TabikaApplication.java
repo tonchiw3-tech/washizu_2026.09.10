97065d304a0bff7f6f1a62d658d0ce2ad14f80f4
tonchiw3-tech <tonchi.w.3@gmail.com>
2026-09-26 19:25:37 +0900

    Spring BootとMySQLの初期環境を構築

---
+++ tabika/src/main/java/com/tabika/TabikaApplication.java
@@ -0,0 +1,13 @@
+package com.tabika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabikaApplication.class, args);
+	}
+
+}
---
+++ tabika/src/test/java/com/tabika/TabikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
